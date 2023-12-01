7545a0d0e0dcea6efd16cfcf932cc067caa89661
Krystian <krystian14d@gmail.com>
2023-12-01 09:42:10 +0100

    jdbc template

---
+++ sdjpa-jdbc-template/src/main/java/guru/springframework/jdbc/domain/Author.java
@@ -0,0 +1,27 @@
+package guru.springframework.jdbc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Transient
+    private List<Book> books;
+}
