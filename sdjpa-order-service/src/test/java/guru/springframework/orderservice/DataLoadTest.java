a7ea8fe00ad5fd2c2f6fbb60482cf9a9941da61b
Krystian <krystian14d@gmail.com>
2023-12-11 20:57:01 +0100

    transactions

--- sdjpa-order-service/src/test/java/guru/springframework/orderservice/DataLoadTest.java
+++ sdjpa-order-service/src/test/java/guru/springframework/orderservice/DataLoadTest.java
@@ -41,6 +41,20 @@ public class DataLoadTest {
     ProductRepository productRepository;
 
     @Test
+    void testDBLock() {
+        Long id = 55l;
+
+        OrderHeader orderHeader = orderHeaderRepository.findById(id).get();
+
+        Address billTo = new Address();
+        billTo.setAddress("Bill me");
+        orderHeader.setBillToAddress(billTo);
+        orderHeaderRepository.saveAndFlush(orderHeader);
+
+        System.out.println("I updated the order");
+    }
+
+    @Test
     void testN_PlusOneProblem() {
 
         Customer customer = customerRepository.findCustomerByCustomerNameIgnoreCase(TEST_CUSTOMER).get();
