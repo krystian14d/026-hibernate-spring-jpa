e29afc21a187084727765b381f16f5579dd57b91
Krystian <krystian14d@gmail.com>
2023-12-14 12:05:50 +0100

    validation

--- sdjpa-order-service/src/main/java/guru/springframework/orderservice/domain/Address.java
+++ sdjpa-order-service/src/main/java/guru/springframework/orderservice/domain/Address.java
@@ -1,8 +1,10 @@
 package guru.springframework.orderservice.domain;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.Objects;
 
@@ -11,9 +13,13 @@ import java.util.Objects;
 @Embeddable
 public class Address {
 
+    @Size(max = 30)
     private String address;
+    @Size(max = 30)
     private String city;
+    @Size(max = 30)
     private String state;
+    @Size(max = 30)
     private String zipCode;
 
     @Override
--- sdjpa-order-service/src/main/java/guru/springframework/orderservice/domain/Customer.java
+++ sdjpa-order-service/src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -13,13 +17,17 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-public class Customer extends BaseEntity{
-    private String customerName;
+public class Customer extends BaseEntity {
 
+    @Size(max = 50)
+    private String customerName;
+    @Valid
     @Embedded
     private Address address;
-
+    @Size(max = 20)
     private String phone;
+    @Size(max = 255)
+    @Email
     private String email;
     @Version
     private Integer version;
